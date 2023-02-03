f1306e442f9a55fd8a5cf0e1d6b9de2b3dd96a4d
Neil Gudwala <neil.gudwala@gmail.com>
2023-02-03 20:11:46 +0530

    Completed module 2

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+from django.contrib import admin
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
